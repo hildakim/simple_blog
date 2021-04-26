b2414ba589ea59a93b68062a22eba25c77584972
hildakim <pinkredjohn@gmail.com>
2021-04-26 18:47:54 +0900

    create, update

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Blog
+from django.utils import timezone
 
 # Create your views here.
 def home(request):
@@ -10,3 +11,27 @@ def detail(request, id):
     recipePost = get_object_or_404(Blog, pk = id)
     return render(request, 'detail.html', {'recipePost':recipePost})
     
+def new(request):
+    return render(request, 'new.html')
+
+def create(request):
+    new_blog = Blog()
+    new_blog.food_title = request.POST['title']
+    new_blog.nickname = request.POST['writer']
+    new_blog.recipe = request.POST['body']
+    new_blog.upload_date = timezone.now()
+    new_blog.save()
+    return redirect('detail', new_blog.id)
+
+def edit(request, id):
+    edit_blog = Blog.objects.get(id = id)
+    return render(request, 'edit.html', {'blog':edit_blog})
+
+def update(request, id):
+    update_blog = Blog.objects.get(id = id)
+    update_blog.food_title = request.POST['title']
+    update_blog.nickname = request.POST['writer']
+    update_blog.recipe = request.POST['body']
+    update_blog.upload_date = timezone.now()
+    update_blog.save()
+    return redirect('detail', update_blog.id)
--- blogproject/urls.py
+++ blogproject/urls.py
@@ -21,4 +21,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name="home"),
     path('<str:id>', detail, name="detail"),
+    path('new/', new, name="new"),
+    path('create/', create, name="create"),
+    path('edit/<str:id>', edit, name="edit"),
+    path('update/<str:id>', update, name="update"),
 ]
